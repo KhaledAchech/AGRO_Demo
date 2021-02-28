e79b63a9b56e2ff113882466173337e3920a8584
Khaled <achechkhaled@gmail.com>
2021-02-28 14:43:07 +0100

    Main Menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+  
+  public void PlayGame()
+  {
+     SceneManager.LoadScene("Prototype"); 
+  }
+
+   public void NewGame()
+  {
+     SceneManager.LoadScene("Layout Groups"); 
+  }
+
+  public void QuitGame()
+  {
+    Application.Quit();
+  }
+}
---
+++ Assets/NewPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewPlayer : MonoBehaviour
+{
+    public void PlayGame()
+  {
+     SceneManager.LoadScene("Prototype"); 
+  }
+
+   public void Cancel()
+  {
+     SceneManager.LoadScene("Menu 3D"); 
+  }
+}
