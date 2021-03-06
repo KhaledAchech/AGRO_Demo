97db10ac4ad257a6a0d82cf3bb73a2db07f569ed
Khaled <achechkhaled@gmail.com>
2021-03-06 13:16:02 +0100

    Player and Enemies UI

---
+++ Assets/BillBoard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    public Transform cam;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
